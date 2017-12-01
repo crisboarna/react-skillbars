8ca131c3daa8dea4c1301971a35dabeb73f95a75
leviathan <cristian.boarna@gmail.com>
2017-12-01 18:16:48 +0200

    ci(GH Pages): Changed demo website location for GH Pages

--- config/paths.js
+++ config/paths.js
@@ -41,7 +41,7 @@ function getServedPath(appPackageJson) {
 // config after eject: we're in ./config/
 module.exports = {
   dotenv: resolveApp('.env'),
-  pageBuild: resolveApp('build-page'),
+  pageBuild: resolveApp('docs'),
   moduleBuild: resolveApp('build-module'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
